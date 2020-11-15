b4a761185ba4c14a7f4117f31e565e4f3c3b9f14
webwebweb5 <68845415+webwebweb5@users.noreply.github.com>
2020-11-15 15:55:51 +0700

    Add files via upload

---
+++ สอบ 60/script/script.js
@@ -0,0 +1,14 @@
+let code = {
+    code: 10,
+    status: "complete"
+};
+
+var id = prompt("The code is 6+4 :");
+switch(parseInt(id)){
+    case 10:
+        alert(code);
+        break;
+    default:
+        alert("Invalid code")
+}
+
